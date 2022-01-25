4d59ed90b92f67a6e8dc2ef892a1fef21f9a92cf
Dennis Au <dennis.hirofumi.au@gmail.com>
2022-01-24 17:09:23 -0800

    Adding avatar app to page

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,25 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <script src="https://unpkg.com/@croquet/croquet@1.0" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      <main>  
+        <div className="split left">  
+            <div className="right-justified">
+                <Header title="Welcome to my app!" />
+                <p className="description">
+                    Get started by editing <code>pages/index.js</code>
+                </p>
+            </div>
+        </div>
+
+        <div className="split right">
+            <div className="left-justified">
+                <iframe src="https://croquet.io/avatars/index.html?q=cxk8m2wrhx" width="400" height="400" />
+            </div>
+        </div>
       </main>
 
       <Footer />
